0e7ba6f4de00e299877e4a93ba7a6cdae9be5bca
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-11-01 11:47:46 -0700

    Complete prettier setup

---
+++ script.js
@@ -0,0 +1,13 @@
+// Using prettier setup https://www.youtube.com/watch?v=DqfQ4DPnRqI
+// First steps to build project environment:
+
+// 1. Initiate NPM to create JSON file:
+// npm init -y
+
+// 2. Install prettier:
+// npm i --save-dev --save-exact prettier
+
+// 3. create .prettierignore file and add node_modules
+
+// 4. If desired, create a .prettierrc file to indicate preferences. I'll leave it in the stock configuration
+// 5. If needed, configure with esLint
